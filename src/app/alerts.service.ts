fd24e85292f50f874f75b0460a5abf377a8e5c3c
Farid ullah <farid.bsse2344@iiu.edu.pk>
2019-02-15 23:53:53 -0800

    added angular material and flex-layout packages. Also added alert component and alert service

---
+++ src/app/alerts.service.ts
@@ -0,0 +1,115 @@
+import { Injectable } from "@angular/core";
+import { MatDialog } from "@angular/material";
+import { Subject, Observable } from "rxjs";
+import { RVAlertAction, RVAlert } from "./alerts.common";
+import { AlertComponent } from "./alert/alert.component";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AlertService {
+  private static _instance: AlertService = null;
+  public static shared(): AlertService {
+    return AlertService._instance;
+  }
+
+  private alerts: Subject<RVAlert>;
+  private queue: RVAlert[];
+
+  constructor(protected dialog: MatDialog) {
+    if (AlertService._instance == null) {
+      AlertService._instance = this;
+    }
+
+    this.alerts = new Subject<RVAlert>();
+    this.queue = [];
+
+    this.alerts.subscribe((a: RVAlert) => {
+      console.log("Alert is subscribed.");
+      this.present(a);
+    });
+  }
+
+  private present(alert: RVAlert) {
+    const dialogRef = this.dialog.open(AlertComponent, {
+      disableClose: true,
+      backdropClass: "alert-backdrop"
+    });
+    dialogRef.componentInstance.alert = alert;
+
+    dialogRef.afterClosed().subscribe((result: RVAlertAction) => {
+      console.log(result);
+      this.checkQueue();
+      // result.alert.subject.next(result);
+      // result.alert.subject.complete();
+    });
+  }
+
+  private checkQueue() {
+    console.log(this.queue.length);
+    if (this.queue.length == 0) return;
+    if (this.dialog.openDialogs.length > 0) return;
+    console.log(this.queue.length);
+    this.alerts.next(this.queue.pop());
+  }
+
+  success(title: string, message: string): Observable<RVAlertAction> {
+    console.log("Success");
+    const alert = new RVAlert("success", title, message);
+
+    this.queue.push(alert);
+    this.checkQueue();
+
+    return alert.subject.asObservable();
+  }
+
+  error(title: string, message: string): Observable<RVAlertAction> {
+    const alert = new RVAlert("error", title, message);
+
+    this.queue.push(alert);
+    this.checkQueue();
+
+    return alert.subject.asObservable();
+  }
+
+  info(title: string, message: string): Observable<RVAlertAction> {
+    const alert = new RVAlert("info", title, message);
+
+    this.queue.push(alert);
+    this.checkQueue();
+
+    return alert.subject.asObservable();
+  }
+
+  warn(title: string, message: string): Observable<RVAlertAction> {
+    const alert = new RVAlert("warning", title, message);
+
+    this.queue.push(alert);
+    this.checkQueue();
+
+    return alert.subject.asObservable();
+  }
+
+  confirm(title: string): Observable<RVAlertAction> {
+    const alert = new RVAlert("confirm", title);
+
+    this.queue.push(alert);
+    this.checkQueue();
+
+    return alert.subject.asObservable();
+  }
+
+  confirmWithInput(
+    title: string,
+    label?: string,
+    required?: boolean
+  ): Observable<RVAlertAction> {
+    const alert = new RVAlert("confirmWithInput", title, "", required);
+    alert.placeholder = label || title;
+
+    this.queue.push(alert);
+    this.checkQueue();
+
+    return alert.subject.asObservable();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import {
+  MatFormFieldModule,
+  MatButtonModule,
+  MatIconModule
+} from "@angular/material";
 
-import { AppComponent } from './app.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { AppComponent } from "./app.component";
+import { AlertComponent } from "./alert/alert.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, AlertComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+
+    // Material Components
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+
+    // Flex Layout
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
